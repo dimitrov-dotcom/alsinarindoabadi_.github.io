2403ac7e87b0aa258cf4915d12a14afa2e8ab821
Dimitrov <dimitrov@users.noreply.github.com>
2025-12-24 16:01:32 +0800

    Add gallery styles and lightbox

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+// GALLERY LIGHTBOX
+function openLightbox(img) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  lightboxImg.src = img.src;
+  lightbox.style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
