1de364c5c23582980e2cff9cc8cff86cc786e258
Dimitrov <dimitrov@users.noreply.github.com>
2025-12-27 23:31:50 +0800

    Add hero slider with auto animation

--- script.js
+++ script.js
@@ -135,3 +135,14 @@ function closeLightbox() {
 document.addEventListener("keydown", e => {
   if (e.key === "Escape") closeLightbox();
 });
+/* ================= HERO SLIDER AUTO ================= */
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".hero-slide");
+  let index = 0;
+
+  setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+  }, 5000); // 5 detik
+});
